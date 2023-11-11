e7d47e7702eac1eff0e24b723aef49ccf8a122e0
hydaline-code <djougancharlen@gmail.com>
2023-11-11 17:46:44 +0100

    fix linters

--- morsecode.rb
+++ morsecode.rb
@@ -1,27 +1,23 @@
-# frozen_string_literal: true
+MORSE_CODE_HASH_TABLE = {
+  '.-' => 'A', '-...' => 'B', '-.-.' => 'C', '-..' => 'D', '.' => 'E',
+  '..-.' => 'F', '--.' => 'G', '....' => 'H', '..' => 'I', '.---' => 'J',
+  '-.-' => 'K', '.-..' => 'L', '--' => 'M', '-.' => 'N', '---' => 'O',
+  '.--.' => 'P', '--.-' => 'Q', '.-.' => 'R', '...' => 'S', '-' => 'T',
+  '..-' => 'U', '...-' => 'V', '.--' => 'W', '-..-' => 'X', '-.--' => 'Y', '--..' => 'Z'
+}.freeze
 
-# MorseDecoder decodes Morse code into plain text.
-class MorseDecoder
-  MORSE = { '.-' => 'A', '-...' => 'B', '-.-.' => 'C', '-..' => 'D', '.' => 'E',
-            '..-.' => 'F', '--.' => 'G', '....' => 'H', '..' => 'I', '.---' => 'J',
-            '-.-' => 'K', '.-..' => 'L', '--' => 'M', '-.' => 'N', '---' => 'O',
-            '.--.' => 'P', '--.-' => 'Q', '.-.' => 'R', '...' => 'S', '-' => 'T',
-            '..-' => 'U', '...-' => 'V', '.--' => 'W', '-..-' => 'X', '-.--' => 'Y',
-            '--..' => 'Z' }.freeze
-
-  def decode_char(morse_char)
-    MORSE[morse_char]
-  end
-
-  def decode_word(morse_word)
-    morse_word.split(' ').map { |morse_char| decode_char(morse_char) }.join
-  end
-
-  def decode_message(morse_message)
-    morse_message.split('   ').map { |morse_word| decode_word(morse_word) }.join(' ')
-  end
+def decode_char(morse_char)
+  MORSE_CODE_HASH_TABLE[morse_char]
 end
 
-message_from_bottle = '.-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...'
-decoder = MorseDecoder.new
-decoded_message = decoder.decode_message(message_from_bottle)
+def decode_word(morse_word)
+  word_array = morse_word.split.map { |morse_char| decode_char(morse_char) }
+  word_array.join
+end
+
+def decode(morse_code)
+  the_word = morse_code.split('   ').map { |morse_word| decode_word(morse_word) }.join(' ')
+  puts "#{the_word} "
+end
+
+decode('.-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...')
