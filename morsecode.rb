768300d19395240b63c4901b9d32f003a812399a
hydaline-code <djougancharlen@gmail.com>
2023-11-11 17:37:39 +0100

    fix linters

--- morsecode.rb
+++ morsecode.rb
@@ -1,22 +1,27 @@
+# frozen_string_literal: true
+
+# MorseDecoder decodes Morse code into plain text.
 class MorseDecoder
-  MORSE = { ".-" => "A", "-..." => "B", "-.-." => "C", "-.." => "D", "." => "E",
-            "..-." => "F", "--." => "G", "...." => "H", ".." => "I", ".---" => "J",
-            "-.-" => "K", ".-.." => "L", "--" => "M", "-." => "N", "---" => "O",
-            ".--." => "P", "--.-" => "Q", ".-." => "R", "..." => "S", "-" => "T",
-            "..-" => "U", "...-" => "V", ".--" => "W", "-..-" => "X", "-.--" => "Y",
-            "--.." => "Z" }.freeze
+  MORSE = { '.-' => 'A', '-...' => 'B', '-.-.' => 'C', '-..' => 'D', '.' => 'E',
+            '..-.' => 'F', '--.' => 'G', '....' => 'H', '..' => 'I', '.---' => 'J',
+            '-.-' => 'K', '.-..' => 'L', '--' => 'M', '-.' => 'N', '---' => 'O',
+            '.--.' => 'P', '--.-' => 'Q', '.-.' => 'R', '...' => 'S', '-' => 'T',
+            '..-' => 'U', '...-' => 'V', '.--' => 'W', '-..-' => 'X', '-.--' => 'Y',
+            '--..' => 'Z' }.freeze
 
-  def self.decode_char(morse_code)
-    MORSE[morse_code]
+  def decode_char(morse_char)
+    MORSE[morse_char]
   end
 
-  def self.decode_word(morse_word)
-    morse_word.split(" ").map { |morse_char| decode_char(morse_char) }.join
+  def decode_word(morse_word)
+    morse_word.split(' ').map { |morse_char| decode_char(morse_char) }.join
   end
 
-  def self.decode_message(morse_message)
-    morse_message.split("   ").map { |morse_word| decode_word(morse_word) }.join(" ")
+  def decode_message(morse_message)
+    morse_message.split('   ').map { |morse_word| decode_word(morse_word) }.join(' ')
   end
 end
 
-message_from_bottle = ".-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ..."
+message_from_bottle = '.-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...'
+decoder = MorseDecoder.new
+decoded_message = decoder.decode_message(message_from_bottle)
